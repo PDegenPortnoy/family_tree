7238f6b0563135181dd92bc203ff5029419c75a8
Peter Degen-Portnoy <pdegen-portnoy@helium.com>
2012-08-02 20:41:09 -0400

    Initial commit after generating project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FamilyTree::Application.config.secret_token = '936eda55271f93b71e6b0cdc5032e322530d978b68b040f9aec4bdbd547713932a64c609daecef640ca58d83401eb2802616f5798c348a1d5f8dda5378da55b8'
